4b60e3e815ac683997a9f297f49133cf0ca75cc1
Pragati <pragatikhard@gmail.com>
2025-01-04 22:58:12 +0530

    constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+// Named Export 
+
+export const CDN_URL =
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+    "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.6161&lng=73.7286&restaurantId="
+
+//  Can not write in such way, It will throw an error-
+//  export default CDN_URL;
+//  export default LOGO_URL;
