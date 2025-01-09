a107e7f22229f0346438ba3aa8760865322be656
Pragati <pragatikhard@gmail.com>
2025-01-09 19:25:41 +0530

    Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -20,6 +20,9 @@ const Body = () => {
         const data = await fetch(
             "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING")
 
+        // Resolve corsproxy.io. 
+        // " https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&l ng=77.62448069999999&page_type=DESKTOP_WEB_LISTING "
+
         const json = await data.json();
         console.log("json data", json)
 
