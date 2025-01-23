3b52c9bbc4200b04a7f2b3b05e0e0aa6f96b0f39
Pragati <pragatikhard@gmail.com>
2025-01-24 00:17:09 +0530

    UserFunctional.js | Functional component

---
+++ src/components/UserFunctional.js
@@ -0,0 +1,33 @@
+import { useState,useEffect } from "react";
+
+// const UserFunctional = (props) => {
+// OR 
+const UserFunctional = ({ name }) => {
+    const [count, setCount] = useState(0);
+    const [count2] = useState(1);
+
+    useEffect(() => {
+        // const timer = setInterval(() => {
+        //     console.log("NAMASTE REACT OP Functional Comp.")
+        // }, 1000);
+
+        // console.log("useEffect")
+
+        // return () => {
+        //     clearInterval(timer);
+        //     console.log("useEffect Return")
+        // };
+    }, [])
+
+    return (
+        <div className="user-card">
+            <h1>Count Function- {count}</h1>
+            <h1>Count2 Function- {count2}</h1>
+            <h2>Name:{name}</h2>
+            <h3>Location: Niwari</h3>
+            <h4>Contact: Pragatikhard@gmail.com</h4>
+        </div>
+    )
+}
+
+export default UserFunctional;
