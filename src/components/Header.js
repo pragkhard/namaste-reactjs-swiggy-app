9358705de4d3a29b3d7d781370cb3200f8f39b9c
Pragati <pragatikhard@gmail.com>
2025-01-24 19:47:22 +0530

    Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,35 +17,35 @@ const Header = () => {
     }, [btnNameReact]);
 
     return (
-        <div className="header">
+        <div className="header flex justify-between  bg-pink-100 shadow-lg m-2  sm:bg-yellow-50 lg:bg-green-50">
             <div className="logo-container">
                 {/* <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" /> */}
-                <img className="logo" src={LOGO_URL} />
+                <img className="logo w-40" src={LOGO_URL} />
             </div>
 
-            <div className="nav-items">
-                <ul>
-                    <li>
+            <div className="nav-items flex items-center">
+                <ul className='flex p-4 m-4'>
+                    <li className='px-4'>
                         Online Status: {onlineStatus ? "✅" : "🔴"}
                     </li>
-                    <li >
+                    <li className='px-4'>
                         <Link to="/">
                             Home
                         </Link>
                     </li>
-                    <li >
+                    <li className='px-4'>
                         <Link to="/about">
                             About us
                         </Link>
 
                     </li>
-                    <li >
+                    <li className='px-4'>
                         <Link to="/contact">Contact us</Link>
                     </li>
-                    <li >
+                    <li className='px-4'>
                         <Link to="/grocery">Grocery</Link>
-                    </li>              
-                    <li>Cart</li>
+                    </li>
+                    <li className='px-4'>Cart</li>
                     <button className='Login' onClick={() => {
                         btnNameReact == "Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login")
                     }}>{btnNameReact}</button>
