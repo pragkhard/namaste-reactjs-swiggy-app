ffb427a07fa7047e448ac73fabbd8e963481130b
Pragati <pragatikhard@gmail.com>
2025-01-24 19:47:39 +0530

    RestaurantCard.js

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -6,15 +6,17 @@ const RestaurantCard = (props) => {
     const { cloudinaryImageId, name, cuisines, avgRating, costForTwo ,deliveryTime} = resData?.info;
     // const { deliveryTime } = resName.info.sla
     return (
-        <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+        <div className="res-card m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-400" 
+        // style={{ backgroundColor: "#f0f0f0" }}
+        >
             {/* <img className='res-logo' src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId} alt="" /> */}
 
-            <img className='res-logo' src={CDN_URL + cloudinaryImageId} alt="" />
+            <img className='res-logo rounded-lg' src={CDN_URL + cloudinaryImageId} alt="" />
 
-            <h3>{name}</h3>
+            <h3 className='font-bold py-4 text-xl'>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating}</h4>
-            <h4>{costForTwo / 100} FOR TWO</h4>
+            {/* <h4>{costForTwo / 100} FOR TWO</h4> */}
             {/* <h4>{deliveryTime}</h4> */}
         </div>
     )
