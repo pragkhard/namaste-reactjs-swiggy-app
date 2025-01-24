9da67653f95365906364e65b0fbd035c5b338ae0
Pragati <pragatikhard@gmail.com>
2025-01-24 17:15:32 +0530

    RestaurantMenu.js

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,41 +1,50 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 // useParam hook  to get the dymanic ids
 import { useParams } from "react-router";
-import { MENU_API } from "../utils/constants";
+// import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 
 const RestaurantMenu = () => {
-    const [resInfo, setResInfo] = useState(null);
+    // const [resInfo, setResInfo] = useState(null);
     // const params = useParams()
     // console.log(params)
 
-    // destructuring
+    // Destructuring
     const { resId } = useParams()
     console.log("Destructuring resId", resId)
 
-    useEffect(() => {
-        fetchMenu()
-    })
 
-    const fetchMenu = async () => {
-        // const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.6161&lng=73.7286&restaurantId=14780&catalog_qa=undefined&submitAction=ENTER")
+    // create custom hook
+    const resInfo = useRestaurantMenu(resId)
 
-        // const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.6161&lng=73.7286&restaurantId="+resId+"&catalog_qa=undefined&submitAction=ENTER")
 
-        // Here i am adding the resId 
-        // using resId - 14780
+    // useEffect(() => {
+    //     fetchMenu()
+    // })
 
-        const data = await fetch(MENU_API + resId)
-        const json = await data.json()
+    // const fetchMenu = async () => {
+    // const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.6161&lng=73.7286&restaurantId=14780&catalog_qa=undefined&submitAction=ENTER")
 
-        console.log(json);
-        setResInfo(json.data)
-    }
+    // const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.6161&lng=73.7286&restaurantId="+resId+"&catalog_qa=undefined&submitAction=ENTER")
+
+    // Here i am adding the resId 
+    // using resId - 14780
+
+    //     const data = await fetch(MENU_API + resId)
+    //     const json = await data.json()
+
+    //     console.log(json);
+    //     setResInfo(json.data)
+    // }
 
     if (resInfo == null) return <Shimmer />
+
     const { name, cuisines, costForTwo } = resInfo?.cards[2]?.card?.card?.info;
     const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[4]?.card?.card;
+
+    console.log(itemCards)
 
     return (
         <div>
