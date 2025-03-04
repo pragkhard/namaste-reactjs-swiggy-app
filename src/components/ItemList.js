7bc3ae5f2b649d347dc02a1668a1767df4315f27
Pragati <pragatikhard@gmail.com>
2025-03-04 23:30:13 +0530

    ItemList.js | accordian body

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,7 @@
 import { CDN_URL } from "../utils/constants";
-const ItemList = ({ items }) => {
+const ItemList = ({ items, dummy }) => {
     // console.log("items", items)
+    // console.log(dummy)
     return (
         <div>
             {/* Category Item */}
