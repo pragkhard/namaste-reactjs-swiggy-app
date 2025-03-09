e447d129fa06398e83846b57d85c0e1c1812ce8e
Pragati <pragatikhard@gmail.com>
2025-03-09 12:34:22 +0530

    Cart.js | Redux

---
+++ src/components/Cart.js
@@ -0,0 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
+import ItemList from "./ItemList";
+import { clearCart } from "../utils/cartSlice";
+
+const Cart = () => {
+    const cartItems = useSelector((store) => store.cart.items);
+    console.log(cartItems);
+
+    // const store = useSelector((store) => store);
+    // const cartItems = store.cart.items;
+
+
+    const dispatch = useDispatch();
+
+    const handleClearCart = () => {
+        dispatch(clearCart());
+    };
+    return (
+        <div className="text-center m-4 p-4">
+            <h1 className="text-2xl font-bold">Cart</h1>
+            <div className="w-6/12 m-auto">
+                {/* build the feature of clear cart here- */}
+                <button
+                    className=" p-2 m-2 bg-black text-white rounded-lg"
+                    onClick={handleClearCart}
+                >
+                    Clear Cart
+                </button>
+
+                {/* If the items is 0 you can just show /display a message add items to the cart */}
+                {cartItems?.length === 0 && (
+                    <h1> Cart is empty. Add Items to the cart!</h1>
+                )}
+                <ItemList items={cartItems} />
+            </div>
+
+        </div>
+    )
+}
+
+export default Cart;
